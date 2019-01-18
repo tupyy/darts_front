db917c6a41b838983b84760cf7b799416897ba31
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-01-18 10:19:29 +0100

    add game storage service

---
+++ src/app/game/services/game-storage.service.ts
@@ -0,0 +1,24 @@
+import {Inject, Injectable} from '@angular/core';
+import {LOCAL_STORAGE, StorageService} from 'angular-webstorage-service';
+import {Game} from '../engine/game';
+import {StandardGame} from '../engine/standard-game';
+
+const STORAGE_KEY = 'my-awesome-game';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GameStorageService {
+
+    constructor(@Inject(LOCAL_STORAGE) private localStorage: StorageService) {
+    }
+
+    public saveGame(game: Game) {
+        this.localStorage.set(STORAGE_KEY, JSON.stringify(game));
+        console.log(JSON.stringify(game));
+    }
+
+    public deleteGame() {
+        this.localStorage.remove(STORAGE_KEY);
+    }
+}
