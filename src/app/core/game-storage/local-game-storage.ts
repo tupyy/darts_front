cfbd0fb1e5145c0921ca00f711698798296d23fa
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-01-25 09:59:00 +0100

    remove unused imports

--- src/app/core/game-storage/local-game-storage.ts
+++ src/app/core/game-storage/local-game-storage.ts
@@ -1,7 +1,6 @@
 import {Inject, Injectable} from '@angular/core';
 import {LOCAL_STORAGE, StorageService} from 'ngx-webstorage-service';
 import {Game} from '@app/engine/game';
-import {CoreService} from '@app/core/core.service';
 
 const STORAGE_KEY = 'my-awesome-game';
 
