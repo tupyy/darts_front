790d6aab0db598bb91c85dec11a42159c75c1a5b
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-04 14:01:07 +0100

    add hasChanged observable

--- src/app/model/engine/standard-move.ts
+++ src/app/model/engine/standard-move.ts
@@ -1,14 +1,18 @@
 import {Move} from './move';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 export class StandardMove implements Move {
     id: number;
     playerId: number;
     shoots: number[];
 
+    private _hasChanged = new BehaviorSubject<number>(0);
+
     static fromJSON(moveJSON): StandardMove {
         const move = Object.create(StandardMove.prototype);
         return Object.assign(move, moveJSON);
     }
+
     constructor(id: number, playerId: number, shoots?: number[]) {
         this.id = id;
         this.playerId = playerId;
@@ -30,6 +34,7 @@ export class StandardMove implements Move {
 
     setScore(shootId: number, value: number) {
         this.shoots[shootId - 1] = value;
+        this._hasChanged.next(shootId - 1);
     }
 
     public getTotalScore(): number {
@@ -38,6 +43,10 @@ export class StandardMove implements Move {
             sum += val;
         });
         return sum;
+    }
+
+    public hasChanged(): Observable<number> {
+        return this._hasChanged.asObservable();
     }
 
     public clone() {
