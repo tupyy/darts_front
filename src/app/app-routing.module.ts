5b4489ed00709a5612aa43f3bee92fe97d8bb995
cosmin <cosmin@home.net>
2019-02-07 18:50:08 +0100

    redirect to index when 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
-import {LoginComponent} from './views/login/login.component';
 
 const appRoute: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
+    {path: '*', redirectTo: '/'}
 ];
 
 @NgModule({
