8319263ba304dd4d6b1b488d1ab713f4a0d1f194
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-07 13:28:08 +0100

    clean class of ununsed methods

--- src/app/views/game/components/standard/standard.component.ts
+++ src/app/views/game/components/standard/standard.component.ts
@@ -1,18 +1,7 @@
-import {BehaviorSubject, Observable} from 'rxjs';
-
 export abstract class StandardComponent {
-
-    protected canNext$ = new BehaviorSubject<boolean>(false);
 
     constructor() {
     }
 
     public abstract reset();
-
-    public abstract onNext();
-
-
-    public canNext(): Observable<boolean> {
-        return this.canNext$.asObservable();
-    }
 }
