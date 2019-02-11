9fc3dd52912552aa5c0054b46a0a02eeeb8190ea
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-11 15:29:13 +0100

    add select to choose from gametype list

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -3,6 +3,7 @@ import {BehaviorSubject, Observable} from 'rxjs';
 import {Game} from '@app/engine/game';
 import {GameType, StandardGame, StandardPlayer} from '@app/engine/index';
 import {GameStorageService} from '@app/core/game-storage/game-storage.service';
+import {Standard501Game} from '@app/engine/standard-501-game';
 
 @Injectable({
     providedIn: 'root'
@@ -24,17 +25,26 @@ export class CoreService {
     }
 
     public createGame(gameType: GameType, playersNames: string[]) {
-        if (gameType === GameType.Standard) {
-            const players = [];
-            for (let i = 0; i < playersNames.length; i++) {
-                players.push(new StandardPlayer(i, playersNames[i]));
-            }
-            const newGame = new StandardGame(players);
-            this.currentGameID = newGame.id;
-            this.currentGame.next(newGame);
-            this.games.push(newGame);
-            this.gameStorageService.setGame(this.getCurrentGame());
+        const players = [];
+        for (let i = 0; i < playersNames.length; i++) {
+            players.push(new StandardPlayer(i, playersNames[i]));
         }
+
+        let newGame: Game;
+        switch (gameType) {
+            case(GameType.Standard_301): {
+                newGame = new StandardGame(players);
+                break;
+            }
+            case (GameType.Standard_501): {
+                newGame = new Standard501Game(players);
+                break;
+            }
+        }
+        this.currentGameID = newGame.id;
+        this.currentGame.next(newGame);
+        this.games.push(newGame);
+        this.gameStorageService.setGame(this.getCurrentGame());
     }
 
     public getCurrentGame(): Observable<Game> {
