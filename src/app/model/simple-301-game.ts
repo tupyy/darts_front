65bcafea7c9730b7bb07a0d721a0b270afafe4c1
cosmin <cosmin@home.net>
2019-02-13 20:27:13 +0100

    add simple 301 and simple 501 game class

---
+++ src/app/model/simple-301-game.ts
@@ -0,0 +1,50 @@
+import {AbstractGame} from './abstract-game';
+import {Game, GameJSON, Move} from './game';
+import {GameType} from './game-type';
+import {Player} from './player';
+
+/**
+ *  This class implements a standard 301 points game. The difference between classic 301 points game is that
+ *  the game can end without a double or a bulls-eye. The first player to reach 0 wins.
+ */
+export class Simple301Game extends AbstractGame {
+
+    gameType = GameType.Simple_301;
+
+    constructor(players: Player[]) {
+        super(players);
+        players.forEach(player => {
+            player.setTemporaryScore(301);
+            player.commitScore();
+        });
+    }
+
+    // TODO implement
+    fromJSON(gameJSON: GameJSON): Game {
+        return undefined;
+    }
+
+    /**
+     * The first player to reach 0 wins.
+     * @param currentPlayer
+     * @param currentMove
+     */
+    isGameFinished(currentPlayer: Player, currentMove: Move) {
+        return currentPlayer.getScore() === 0;
+    }
+
+    // TODO implement
+    toJSON(): Game {
+        return undefined;
+    }
+
+    updatePlayerScore(player: Player, currentMove: Move) {
+        const oldScore = player.getScore();
+        const moveScore = currentMove.getTotalScore();
+        if (oldScore - moveScore > 0) {
+            player.setTemporaryScore(oldScore - moveScore);
+        }
+    }
+
+
+}
