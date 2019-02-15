fb2bcf5be2afb00aa3a09b3d5794f10633ef1d4a
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-15 10:58:11 +0100

    add standard 501 game description component

---
+++ src/app/views/game/components/game-type-description/standard-501-description.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-standard-501-description',
+    template: `<p>Standard 501</p>`
+})
+export class Standard501DescriptionComponent implements OnInit{
+    constructor() {}
+
+    ngOnInit(): void {
+    }
+}
