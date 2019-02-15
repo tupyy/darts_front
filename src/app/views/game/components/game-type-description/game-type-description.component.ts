12a3b3c644def50bf3a6dc1e6e62b2ca02e029fc
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-15 10:58:48 +0100

    dynamic loading for game description components

--- src/app/views/game/components/game-type-description/game-type-description.component.ts
+++ src/app/views/game/components/game-type-description/game-type-description.component.ts
@@ -1,5 +1,18 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {GameType, GameTypeDescription} from '@app/model/game-type';
+import {
+    AfterViewInit,
+    Component,
+    ComponentFactoryResolver,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
+import {GameType} from '@app/model/game-type';
+import {GameDescriptionDirective} from '../../directives/game-description.directive';
+import {Standard501DescriptionComponent} from './standard-501-description.component';
+import {Standard301DescriptionComponent} from './standard-301-description.component';
 
 @Component({
     selector: 'app-game-type-description',
@@ -10,21 +23,52 @@ export class GameTypeDescriptionComponent implements OnInit, OnChanges {
 
     @Input() gameType: GameType;
     public showDescription = false;
-    public gameTypeDescriptionContent: string;
     private gameTypeName: string;
 
-    private gameTypeDescription = new GameTypeDescription();
+    @ViewChild(GameDescriptionDirective) gameDescriptionComponent: GameDescriptionDirective;
 
-    constructor() {
+    constructor(private componentFactoryResolver: ComponentFactoryResolver) {
     }
 
     ngOnInit() {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        this.showDescription = true;
-        this.gameTypeDescriptionContent = this.gameTypeDescription.getDescription(this.gameType);
-        this.gameTypeName = this.gameTypeDescription.getGameType(this.gameType);
+        if (this.gameType !== undefined) {
+            this.showDescription = true;
+            this.gameTypeName = this.getGameType(this.gameType);
+            this.loadComponent(this.gameType);
+        }
+
     }
+
+    private loadComponent(componentType: GameType) {
+        switch (componentType) {
+            case GameType.Standard_301: {
+                const componentFactory = this.componentFactoryResolver.resolveComponentFactory(Standard301DescriptionComponent);
+                const viewContainerRef =  this.gameDescriptionComponent.viewContainerRef;
+                viewContainerRef.clear();
+                const componentRef = viewContainerRef.createComponent(componentFactory);
+                break;
+            }
+            case GameType.Standard_501: {
+                const componentFactory = this.componentFactoryResolver.resolveComponentFactory(Standard501DescriptionComponent);
+                const viewContainerRef =  this.gameDescriptionComponent.viewContainerRef;
+                viewContainerRef.clear();
+                const componentRef = viewContainerRef.createComponent(componentFactory);
+                break;
+            }
+
+        }
+    }
+
+    private getGameType(gameType: GameType): string {
+        const re = /_/gi;
+        let gameTypeStr: string = GameType[gameType];
+        gameTypeStr = gameTypeStr.replace(re, ' ');
+        return gameTypeStr.charAt(0).toUpperCase() + gameTypeStr.slice(1);
+    }
+
+
 
 }
