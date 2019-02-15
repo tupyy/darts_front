98bee2678b0cea09bb05eda100d5d3a09a797793
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-02-15 10:58:01 +0100

    add standard 301 game description component

---
+++ src/app/views/game/components/game-type-description/standard-301-description.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-standard-301-description',
+    template: `<p>Standard 301</p>`
+})
+export class Standard301DescriptionComponent implements OnInit{
+    constructor() {}
+
+    ngOnInit(): void {
+    }
+}
